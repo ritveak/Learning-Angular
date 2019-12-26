00abf893ff86f9eb09712544461f6479b9877cc3
Ritveak Dugar <ritveakdugar2014244@gmail.com>
2019-12-26 18:26:59 +0530

    started two way binding check docs.

--- ng-firstone/src/app/app.module.ts
+++ ng-firstone/src/app/app.module.ts
@@ -4,13 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { PersonsComponent} from './persons/persons.component';
 import { PersonInputComponent } from './persons/person-input.component';
-
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent, PersonsComponent,PersonInputComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
