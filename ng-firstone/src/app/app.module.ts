43df0513b05cede1201c825f2fd9d0a518dddc7b
Ritveak Dugar <ritveakdugar2014244@gmail.com>
2019-12-27 17:29:13 +0530

    ng-firstone cured

--- ng-firstone/src/app/app.module.ts
+++ ng-firstone/src/app/app.module.ts
@@ -5,13 +5,12 @@ import { AppComponent } from './app.component';
 import { PersonsComponent} from './persons/persons.component';
 import { PersonInputComponent } from './persons/person-input.component';
 import { FormsModule } from '@angular/forms';
-import { AppRoutingModule } from './app-routing-module';
 @NgModule({
   declarations: [
     AppComponent, PersonsComponent, PersonInputComponent
   ],
   imports: [
-    BrowserModule, FormsModule, AppRoutingModule
+    BrowserModule, FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
