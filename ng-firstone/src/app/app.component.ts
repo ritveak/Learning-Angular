3a5a2a563c351bf98457cc36392b1b713d2ec9a1
Ritveak Dugar <ritveakdugar2014244@gmail.com>
2019-12-27 10:46:42 +0530

    Event binding

--- ng-firstone/src/app/app.component.ts
+++ ng-firstone/src/app/app.component.ts
@@ -8,4 +8,8 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'ng-firstone';
   persons = ['Max', 'Man', 'Mona'];
+
+  onPersonCreated( name: string) {
+    this.persons.push(name);
+  }
 }
--- ng-firstone/src/app/persons/person-input.component.ts
+++ ng-firstone/src/app/persons/person-input.component.ts
@@ -1,14 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter} from '@angular/core';
+
 
 @Component({
   selector: 'app-person-input',
   templateUrl: './person-input.component.html',
   styleUrls: ['./person-input.component.css']
 })
-export class PersonInputComponent{
-  enteredName ='';
-  onCreatePerson(){
-    console.log('created a new person '+ this.enteredName);
+export class PersonInputComponent {
+  enteredName = '';
+  @Output() personCreate = new EventEmitter<string>();
+  onCreatePerson()  {
+    console.log('created a new person ' + this.enteredName);
+    this.personCreate.emit(this.enteredName);
     this.enteredName = '';
   }
 }
