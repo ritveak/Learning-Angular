9b29664078edaaf8dc60acafca6a35b09eabf701
Ritveak Dugar <ritveakdugar2014244@gmail.com>
2020-01-02 16:25:49 +0530

    update

--- cmp-databinding-start/src/app/app.component.ts
+++ cmp-databinding-start/src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  serverElements = [];
+  serverElements = [{type : 'server', name : 'Testserver', content: 'Just a test!'}];
 
 
 
